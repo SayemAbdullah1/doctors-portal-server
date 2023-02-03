4e357496bd50b73eec3b85cdff50f4680c0c4638
SayemAbdullah1 <dxsayem0@gmail.com>
2023-02-03 19:35:44 +0600

    add mongodb and create collections

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,22 @@ const app = express();
 //middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1q1hbsc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try {
+        const appointmentOptionsCollection = client.db('doctorsPortalNew').collection('appointmentOptions')
+    } finally {
+
+    }
+}
+run().catch(console.log)
+
 
 app.get('/', async(req, res) =>{
     res.send(`Doctors portal is running`)
